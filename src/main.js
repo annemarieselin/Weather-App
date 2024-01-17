0078ee7c64c991dbec8844513490426986945edb
annemarieselin <156382573+annemarieselin@users.noreply.github.com>
2024-01-17 15:47:43 -0800

    Added automatic Icon Pull from API via JS

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-image" />`;
 
   cityElement.innerHTML = response.data.city;
 
