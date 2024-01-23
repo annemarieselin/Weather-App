b56b899aaaa895959b21390686164cc45ffbf44b
annemarieselin <156382573+annemarieselin@users.noreply.github.com>
2024-01-23 10:13:36 -0800

    Fixed bug in forecast

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ function handleSearchSubmit(event) {
 
 function getForecast(city) {
   let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayForecast);
 }
 
@@ -75,7 +75,7 @@ function displayForecast(response) {
               Tuesday
             </div>
             <img
-              src="{day.condition.icon_url}" />
+              src="${day.condition.icon_url}"
               alt=""
               width="42"
             />
